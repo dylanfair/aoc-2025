0ccd25643e16d2b616a8bb5f6c255c91019d825e
Dylan <carringtonfair.dylan@gmail.com>
2025-11-30 02:17:43 -0500

    prepping for advent of code 2025!

---
+++ src/day01/main.rs
@@ -0,0 +1,14 @@
+use std::{fs::File, io, io::BufRead};
+
+fn main() {
+    part_one("src/day01/test.txt");
+}
+
+fn part_one(path: &str) {
+    let file = File::open(path).unwrap();
+    let lines = io::BufReader::new(file).lines();
+
+    for line in lines {
+        println!("{:?}", line.unwrap());
+    }
+}
