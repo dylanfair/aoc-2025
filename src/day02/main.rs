85df5e1996eda3f2661ada9ee25c733b4af5f6b0
Dylan <carringtonfair.dylan@gmail.com>
2025-12-03 00:22:14 -0500

    have the parsing down

--- src/day02/main.rs
+++ src/day02/main.rs
@@ -1,4 +1,7 @@
-use std::{fs::File, io, io::BufRead};
+use std::{
+    fs::File,
+    io::{self, Read},
+};
 
 fn main() {
     part_one("src/day02/test.txt");
@@ -6,5 +9,24 @@ fn main() {
 
 fn part_one(path: &str) {
     let file = File::open(path).unwrap();
-    let lines = io::BufReader::new(file).lines();
+    let mut input = String::new();
+    io::BufReader::new(file).read_to_string(&mut input).unwrap();
+
+    let ranges: Vec<(&str, &str)> = input
+        .split(',')
+        .into_iter()
+        .map(|range| range.trim_ascii().split_once('-').unwrap())
+        .collect();
+
+    for range in ranges {
+        println!("{} to {}", range.0, range.1);
+        println!("{}", range.0);
+        println!("{}", range.1);
+        let start = range.0.parse::<u64>().unwrap();
+        let end = range.1.parse::<u64>().unwrap();
+
+        for i in start..=end {
+            println!("{i}")
+        }
+    }
 }
