a983ef6a082298c53df516611bd65df79723500a
Dylan <carringtonfair.dylan@gmail.com>
2025-12-04 23:36:59 -0500

    adjusted day04 to use a hashset instead of hashmap

--- src/day04/main.rs
+++ src/day04/main.rs
@@ -1,15 +1,23 @@
-use std::collections::HashMap;
+use std::collections::HashSet;
+use std::time::Instant;
 use std::{
     fs::File,
     io::{self, BufRead},
 };
 
 fn main() {
+    let start1 = Instant::now();
     part_one("src/day04/real.txt");
+    let duration1 = start1.elapsed();
+    println!("Part one time: {:?}", duration1);
+
+    let start2 = Instant::now();
     part_two("src/day04/real.txt");
+    let duration2 = start2.elapsed();
+    println!("Part two time: {:?}", duration2);
 }
 
-fn valid_roll(coord: &(i16, i16), grid: &HashMap<(i16, i16), bool>) -> bool {
+fn valid_roll(coord: &(i16, i16), grid: &HashSet<(i16, i16)>) -> bool {
     const MOVEMENTS: [(i16, i16); 8] = [
         (-1, -1),
         (-1, 1),
@@ -26,12 +34,10 @@ fn valid_roll(coord: &(i16, i16), grid: &HashMap<(i16, i16), bool>) -> bool {
         let row = coord.0 + movement.0;
         let col = coord.1 + movement.1;
 
-        if let Some(roll) = grid.get(&(row, col)) {
-            if *roll {
-                surrounding_rolls += 1;
-                if surrounding_rolls == 4 {
-                    return false;
-                }
+        if grid.contains(&(row, col)) {
+            surrounding_rolls += 1;
+            if surrounding_rolls == 4 {
+                return false;
             }
         }
     }
@@ -42,25 +48,21 @@ fn valid_roll(coord: &(i16, i16), grid: &HashMap<(i16, i16), bool>) -> bool {
 fn part_one(path: &str) {
     let file = File::open(path).unwrap();
     let lines = io::BufReader::new(file).lines();
-    let mut grid = HashMap::new();
+    let mut grid = HashSet::new();
     let mut total = 0;
 
     for (row, line) in lines.enumerate() {
         let rolls = line.unwrap();
         for (column, roll) in rolls.chars().enumerate() {
-            if roll == '.' {
-                grid.insert((row as i16, column as i16), false);
-            } else {
-                grid.insert((row as i16, column as i16), true);
+            if roll == '@' {
+                grid.insert((row as i16, column as i16));
             }
         }
     }
 
-    for (coord, roll) in grid.iter() {
-        if *roll {
-            if valid_roll(coord, &grid) {
-                total += 1;
-            }
+    for coord in grid.iter() {
+        if valid_roll(coord, &grid) {
+            total += 1;
         }
     }
 
@@ -70,16 +72,14 @@ fn part_one(path: &str) {
 fn part_two(path: &str) {
     let file = File::open(path).unwrap();
     let lines = io::BufReader::new(file).lines();
-    let mut grid = HashMap::new();
+    let mut grid = HashSet::new();
     let mut total = 0;
 
     for (row, line) in lines.enumerate() {
         let rolls = line.unwrap();
         for (column, roll) in rolls.chars().enumerate() {
-            if roll == '.' {
-                grid.insert((row as i16, column as i16), false);
-            } else {
-                grid.insert((row as i16, column as i16), true);
+            if roll == '@' {
+                grid.insert((row as i16, column as i16));
             }
         }
     }
@@ -87,13 +87,10 @@ fn part_two(path: &str) {
     let mut prior_total = 0;
     loop {
         let mut coords_to_set_to_false = vec![];
-        for coord in grid.keys() {
-            let roll = grid.get(coord).unwrap();
-            if *roll {
-                if valid_roll(coord, &grid) {
-                    total += 1;
-                    coords_to_set_to_false.push(*coord);
-                }
+        for coord in grid.iter() {
+            if valid_roll(coord, &grid) {
+                total += 1;
+                coords_to_set_to_false.push(*coord);
             }
         }
         if total == prior_total {
@@ -101,7 +98,7 @@ fn part_two(path: &str) {
         }
         prior_total = total;
         for coords in coords_to_set_to_false {
-            grid.insert(coords, false);
+            grid.remove(&coords);
         }
     }
 
