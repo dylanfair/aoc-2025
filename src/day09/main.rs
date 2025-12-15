1b3bfacefad83c6efde4887bb8f4bbf1b1f74b12
Dylan <carringtonfair.dylan@gmail.com>
2025-12-14 21:18:14 -0500

    still struggling

--- src/day09/main.rs
+++ src/day09/main.rs
@@ -13,7 +13,7 @@ fn main() {
     println!("Part one time: {:?}", duration1);
 
     let start2 = Instant::now();
-    part_two("src/day09/test.txt");
+    part_two("src/day09/real.txt");
     let duration2 = start2.elapsed();
     println!("Part two time: {:?}", duration2);
 }
@@ -44,7 +44,6 @@ fn part_one(path: &str) {
             }
         }
     }
-    println!("{max_area}");
 }
 
 fn part_two(path: &str) {
@@ -60,6 +59,10 @@ fn part_two(path: &str) {
     }
 
     let mut segments = vec![];
+    let mut minX = 0;
+    let mut minY = 0;
+    let mut maxX = 0;
+    let mut maxY = 0;
     for i in 0..red_tiles.len() {
         let mut j = i + 1;
         if i == red_tiles.len() - 1 {
@@ -69,9 +72,16 @@ fn part_two(path: &str) {
         let tile2 = red_tiles.get(j).unwrap();
 
         segments.push((*tile1, *tile2));
+
+        minX = minX.min(tile1.0);
+        maxX = maxX.max(tile1.0);
+        minY = minY.min(tile1.1);
+        maxY = maxY.max(tile1.1);
     }
 
     let mut max_area = 0;
+    let mut max_redtile_i = (0, 0);
+    let mut max_redtile_j = (0, 0);
     for i in 0..red_tiles.len() {
         for j in i + 1..red_tiles.len() {
             let red_tile_i = red_tiles.get(i).unwrap();
@@ -84,10 +94,10 @@ fn part_two(path: &str) {
             let segment3 = (*red_tile_j, red_tile_y);
             let segment4 = (red_tile_y, *red_tile_i);
 
-            if is_point_inside(red_tile_i, &segments)
-                && is_point_inside(red_tile_j, &segments)
-                && is_point_inside(&red_tile_x, &segments)
-                && is_point_inside(&red_tile_y, &segments)
+            if point_is_in_poly(red_tile_i, &segments, minX, minY, maxX, maxY)
+                && point_is_in_poly(red_tile_j, &segments, minX, minY, maxX, maxY)
+                && point_is_in_poly(&red_tile_x, &segments, minX, minY, maxX, maxY)
+                && point_is_in_poly(&red_tile_y, &segments, minX, minY, maxX, maxY)
                 && is_segment_inside(&segment1, &segments)
                 && is_segment_inside(&segment2, &segments)
                 && is_segment_inside(&segment3, &segments)
@@ -100,11 +110,48 @@ fn part_two(path: &str) {
                     println!("{:?} and {:?} are valid", red_tile_i, red_tile_j);
                     println!("New max! {area}");
                     max_area = area;
+                    max_redtile_i = *red_tile_i;
+                    max_redtile_j = *red_tile_j;
                 }
             }
+            // if is_point_inside(red_tile_i, &segments)
+            //     && is_point_inside(red_tile_j, &segments)
+            //     && is_point_inside(&red_tile_x, &segments)
+            //     && is_point_inside(&red_tile_y, &segments)
+            //     && is_segment_inside(&segment1, &segments)
+            //     && is_segment_inside(&segment2, &segments)
+            //     && is_segment_inside(&segment3, &segments)
+            //     && is_segment_inside(&segment4, &segments)
+            // {
+            //     let height = red_tile_i.1.abs_diff(red_tile_j.1) + 1;
+            //     let width = red_tile_i.0.abs_diff(red_tile_j.0) + 1;
+            //     let area = height * width;
+            //     if area > max_area {
+            //         println!("{:?} and {:?} are valid", red_tile_i, red_tile_j);
+            //         println!("New max! {area}");
+            //         max_area = area;
+            //         max_redtile_i = *red_tile_i;
+            //         max_redtile_j = *red_tile_j;
+            //     }
+            // }
         }
     }
     println!("{max_area}");
+    println!("{max_area}");
+    println!("tile i: {:?}", max_redtile_i);
+    println!("tile j: {:?}", max_redtile_j);
+    let red_tile_x = (max_redtile_i.0, max_redtile_j.1);
+    let red_tile_y = (max_redtile_j.0, max_redtile_i.1);
+    println!("tile x: {:?}", red_tile_x);
+    println!("tile y: {:?}", red_tile_y);
+    let segment1 = (max_redtile_i, red_tile_x);
+    let segment2 = (red_tile_x, max_redtile_j);
+    let segment3 = (max_redtile_j, red_tile_y);
+    let segment4 = (red_tile_y, max_redtile_i);
+    println!("segement1: {:?}", segment1);
+    println!("segement2: {:?}", segment2);
+    println!("segement3: {:?}", segment3);
+    println!("segement4: {:?}", segment4);
 }
 
 fn is_point_inside(point: &(i64, i64), polygon: &LineSegment) -> bool {
@@ -144,6 +191,39 @@ fn orient(point_a: &(i64, i64), point_b: &(i64, i64), point_c: &(i64, i64)) -> i
     )
 }
 
+fn point_is_in_poly(
+    point: &(i64, i64),
+    polygon: &LineSegment,
+    minX: i64,
+    minY: i64,
+    maxX: i64,
+    maxY: i64,
+) -> bool {
+    if point.0 < minX || point.0 > maxX || point.1 < minY || point.1 > maxY {
+        return false;
+    }
+
+    let mut count = 0;
+    for segment in polygon {
+        if is_between(point, segment) {
+            if point.0 == 3611 && point.1 == 95543 {
+                println!("Count upped - got an is_between {:?}", segment);
+            }
+            return true;
+        }
+        if (segment.0.1 > point.1) != (segment.1.1 > point.1)
+            && point.0
+                < ((segment.1.0 - segment.0.0) * (point.1 - segment.0.1)
+                    / (segment.1.1 - segment.0.1)
+                    + segment.0.0)
+        {
+            count += 1;
+        }
+    }
+
+    if (count & 1) == 1 { true } else { false }
+}
+
 fn intercept(
     point_a: &(i64, i64),
     point_b: &(i64, i64),
@@ -157,7 +237,7 @@ fn intercept(
 
     oa * ob < 0 && oc * od < 0
 
-    // if oa != ob && oc != od {
+    // if oa != oc && ob != od {
     //     return true;
     // }
     //
@@ -185,16 +265,63 @@ fn ray_cast(point: &(i64, i64), polygon: &LineSegment) -> u64 {
     for side in polygon {
         // If on a segment, we can just exit early
         if is_between(point, side) {
+            if point.0 == 3611 && point.1 == 95543 {
+                println!("Count upped - got an is_between {:?}", side);
+            }
             return 1;
         }
+        // if check_overlap(&(*point, point_segment), side) {
+        //     // println!("Got an overlap! {:?} - {:?}", (point, point_segment), side);
+        //     if point.0 == 3611 && point.1 == 95543 {
+        //         println!("Count upped - got a check_overlap {:?}", side);
+        //     }
+        //     count += 1;
+        //     continue;
+        // }
         // Otherwise need to check if inside or outside
         // check if segment crosses
         if intercept(point, &point_segment, &side.0, &side.1) {
+            if point.0 == 3611 && point.1 == 95543 {
+                println!("Count upped - got an intercept {:?}", side);
+            }
             count += 1;
         }
     }
 
+    if point.0 == 3611 && point.1 == 95543 {
+        println!("Count returned was {count}");
+    }
+
     count
+}
+
+fn check_overlap(segment1: &((i64, i64), (i64, i64)), segment2: &((i64, i64), (i64, i64))) -> bool {
+    // Check x
+    let segment1_x_check = segment1.0.0 == segment1.1.0;
+    let segment2_x_check = segment2.0.0 == segment2.1.0;
+    let segment1_to_2_x_check = segment1.0.0 == segment2.0.0;
+
+    if segment1_x_check && segment2_x_check && segment1_to_2_x_check {
+        let segment1_height = segment1.0.1.abs_diff(segment1.1.1);
+        let segment2_height = segment2.0.1.abs_diff(segment2.1.1);
+        if segment1_height > segment2_height {
+            return true;
+        }
+    }
+
+    // Check y
+    let segment1_y_check = segment1.0.1 == segment1.1.1;
+    let segment2_y_check = segment2.0.1 == segment2.1.1;
+    let segment1_to_2_y_check = segment1.0.1 == segment2.0.1;
+    if segment1_y_check && segment2_y_check && segment1_to_2_y_check {
+        let segment1_len = segment1.0.0.abs_diff(segment1.1.0);
+        let segment2_len = segment2.0.0.abs_diff(segment2.1.0);
+        if segment1_len > segment2_len {
+            return true;
+        }
+    }
+
+    false
 }
 
 fn is_between(point: &(i64, i64), side: &((i64, i64), (i64, i64))) -> bool {
@@ -295,5 +422,107 @@ mod tests {
         assert!(is_point_inside(&vertex_point2, &polygon));
         assert!(is_point_inside(&vertex_point3, &polygon));
         assert!(is_point_inside(&vertex_point4, &polygon));
+    }
+
+    #[test]
+    fn alternative_point_test() {
+        let polygon = vec![
+            ((1, 1), (1, 3)),
+            ((1, 3), (3, 3)),
+            ((3, 3), (3, 1)),
+            ((3, 1), (1, 1)),
+        ];
+
+        let minX = 1;
+        let minY = 1;
+        let maxX = 3;
+        let maxY = 3;
+
+        let inside_point = (2, 2);
+        assert!(point_is_in_poly(
+            &inside_point,
+            &polygon,
+            minX,
+            minY,
+            maxX,
+            maxY
+        ));
+
+        let edge_point = (3, 2);
+        assert!(point_is_in_poly(
+            &edge_point,
+            &polygon,
+            minX,
+            minY,
+            maxX,
+            maxY
+        ));
+
+        let other_edge_point = (1, 2);
+        assert!(point_is_in_poly(
+            &other_edge_point,
+            &polygon,
+            minX,
+            minY,
+            maxX,
+            maxY
+        ));
+
+        let outside_point = (0, 2);
+        assert!(!point_is_in_poly(
+            &outside_point,
+            &polygon,
+            minX,
+            minY,
+            maxX,
+            maxY
+        ));
+
+        let other_outside_point = (4, 2);
+        assert!(!point_is_in_poly(
+            &other_outside_point,
+            &polygon,
+            minX,
+            minY,
+            maxX,
+            maxY
+        ));
+
+        let vertex_point1 = (1, 1);
+        let vertex_point2 = (1, 3);
+        let vertex_point3 = (3, 3);
+        let vertex_point4 = (3, 1);
+        assert!(point_is_in_poly(
+            &vertex_point1,
+            &polygon,
+            minX,
+            minY,
+            maxX,
+            maxY
+        ));
+        assert!(point_is_in_poly(
+            &vertex_point2,
+            &polygon,
+            minX,
+            minY,
+            maxX,
+            maxY
+        ));
+        assert!(point_is_in_poly(
+            &vertex_point3,
+            &polygon,
+            minX,
+            minY,
+            maxX,
+            maxY
+        ));
+        assert!(point_is_in_poly(
+            &vertex_point4,
+            &polygon,
+            minX,
+            minY,
+            maxX,
+            maxY
+        ));
     }
 }
